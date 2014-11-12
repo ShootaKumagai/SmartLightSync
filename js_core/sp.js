89f9fb9c694e401ef5c9025519581e928f8d6609
Kumagai Shuta <shoota@9-minutes.com>
2014-11-13 01:01:30 +0900

    SPのJSを外部ファイル化

---
+++ js_core/sp.js
@@ -0,0 +1,21 @@
+//重力加速度を取得して送信
+pre_x=0;
+window.addEventListener("devicemotion", function(evt){
+var x = evt.accelerationIncludingGravity.x;	// 横方向の傾斜
+//var y = evt.accelerationIncludingGravity.y;	// 縦方向の傾斜
+//var z = evt.accelerationIncludingGravity.z;	// 上下方向の傾斜
+var abs_x=Math.abs(pre_x-x)
+if (abs_x>1){
+	pre_x=x;
+	x=Math.round(x);
+	abs_x=Math.round(abs_x);
+	sendMessage(abs_x);
+}
+}, true);
+//初期画面
+function done(){$('#pre').fadeOut()}
+
+//docureaday
+$(document).ready(function(e) {
+  setTimeout  ("done()",1000)
+});
